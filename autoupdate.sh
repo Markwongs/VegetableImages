c5097b7236147c67c592e203bebeef8e955571c7
Markwongs <xwsever@gmail.com>
2021-03-14 20:06:50 +0800

    commited

---
+++ autoupdate.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+cd /home/pi/Documents/VegetableImages
+git init 
+git add .
+git commit -m "commited"
+git push -u  origin main
---
+++ imgCap.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import time
+
+videoCaptureObject = cv.VideoCapture(0)
+result = True
+local_time = time.localtime()
+mon, day = local_time[1], local_time[2]
+while result:
+        ret, frame = videoCaptureObject.read()
+        cv.imwrite(r'/home/pi/Documents/VegetableImages/images/'+'B'+str(mon)+'-'+str(day)+'.jpg', frame)
+        result = False
+videoCaptureObject.release()
+cv.destroyAllWindows()
