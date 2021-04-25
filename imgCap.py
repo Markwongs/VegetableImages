565cea16ee278798951ba6928059ec3c2f16fb8f
Markwongs <xwsever@gmail.com>
2021-04-25 10:59:28 +0800

    commited

--- imgCap.py
+++ imgCap.py
@@ -1,4 +1,4 @@
-import cv2 as cv
+mport cv2 as cv
 import time
 
 print("-"*50)
@@ -9,20 +9,12 @@ capture.set(cv.CAP_PROP_FRAME_HEIGHT, 5000)
 capture.set(cv.CAP_PROP_FRAME_WIDTH, 5000)
 local_time = time.localtime()
 mon, day, hour, min = [local_time[i] for i in range(1, 5)]
-<<<<<<< HEAD
-
 
 while True:
     _, frame = capture.read()
-    cv.imwrite(r"/home/pi/Documents/VegetableImages/images"+'/'+'B'+str(mon)+'-'+str(day) + '-' + str(hour) + ':' + str(min) + '.jpg', frame)
-=======
-
-while True:
-    _, frame = capture.read()
-    cv.imwrite(r"/home/pi/Documents/VegetableImages/images"+'/'+'B'+str(mon)+'-'+str(day) + '-' + str(hour) + ':' + str(min) + '.jpg', frame)
->>>>>>> d4de339613b03e72d0e2d332ffca243ab7b40afe
+    cv.imwrite(r"/home/pi/Documents/VegetableImages/images"+'/'+'B'+str(mon)+'-'+str(day) + '-' + str(hour) + '-' + str(min) + '.jpg', frame)
     break
-                
+                              
 capture.release()
 cv.destroyAllWindows()
 print("Image Captured")
